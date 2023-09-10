bf017f9ae20cab1813ddd708ff5605479847481d
John Moreau <johnmoreau19@gmail.com>
2023-09-10 13:29:57 -0700

    wtf

--- c7api.go
+++ c7api.go
@@ -23,6 +23,8 @@ import (
 // 	r.attempts = 0
 // }
 
+//
+
 // var attemptCount attemptCounter
 
 // Errors will return a custom error type called C7Error if there is an error directly from C7, calling err.Error() on this will return the error message from C7 and the status code.
