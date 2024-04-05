9297948cb3db4fbaf6bc36343ce0f1b5ffec21db
John <johnmoreau19@gmail.com>
2024-04-04 22:06:19 -0700

    Added new user struct

--- c7structs.go
+++ c7structs.go
@@ -437,3 +437,17 @@ type FulfillmentAllItems struct {
 	} `json:"shipped"`
 	PackageCount int `json:"packageCount"`
 }
+
+type NewUser struct {
+	TenantID   string `json:"tenantId"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	TimeZone   string `json:"timeZone"`
+	WeightUnit string `json:"weightUnit"`
+	User       struct {
+		UserID    string `json:"id"`
+		FirstName string `json:"firstName"`
+		LastName  string `json:"lastName"`
+		Email     string `json:"email"`
+	} `json:"user"`
+}
