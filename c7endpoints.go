76dfd592ab4427eeb8803b4c373f4f64ee02a778
John <johnmoreau19@gmail.com>
2024-04-06 10:30:15 -0700

    Made endpoints an optional struct

--- c7endpoints.go
+++ c7endpoints.go
@@ -1,17 +1,29 @@
 package c7api
 
 type Endpoint struct {
-	Auth     string
-	Customer string
-	Order    string
+	Auth           string // "https://api.commerce7.com/v1/account/user" - https://developer.commerce7.com/docs/commerce7-apis
+	Cart           string // "https://api.commerce7.com/v1/cart" - https://developer.commerce7.com/docs/orders
+	Customer       string // "https://api.commerce7.com/v1/customer" - https://developer.commerce7.com/docs/customers
+	CustomerId     string // "https://api.commerce7.com/v1/customer/{:id}" - https://developer.commerce7.com/docs/customers
+	FulfillmentAll string // "https://api.commerce7.com/v1/order/{:id}/fulfillment/all" - https://developer.commerce7.com/docs/fulfillment
+	Fulfillment    string // "https://api.commerce7.com/v1/order/{:id}/fulfillment" - https://developer.commerce7.com/docs/fulfillment
+	Inventory      string // "https://api.commerce7.com/v1/inventory" - https://developer.commerce7.com/docs/inventory
+	InventoryTrans string // "https://api.commerce7.com/v1/inventory-transaction" - https://developer.commerce7.com/docs/inventory
+	Order          string // "https://api.commerce7.com/v1/order" - https://developer.commerce7.com/docs/orders
+	Product        string // "https://api.commerce7.com/v1/product" - https://developer.commerce7.com/docs/products
 }
 
 func GetEndpointsV1() *Endpoint {
-	endpoint := Endpoint{
-		Auth:     "https://api.commerce7.com/v1/account/user",
-		Customer: "",
-		Order:    "",
+	return &Endpoint{
+		Auth:           "https://api.commerce7.com/v1/account/user",
+		Cart:           "https://api.commerce7.com/v1/cart",
+		Customer:       "https://api.commerce7.com/v1/customer",
+		CustomerId:     "https://api.commerce7.com/v1/customer/{:id}",
+		FulfillmentAll: "https://api.commerce7.com/v1/order/{:id}/fulfillment/all",
+		Fulfillment:    "https://api.commerce7.com/v1/order/{:id}/fulfillment",
+		Inventory:      "https://api.commerce7.com/v1/inventory",
+		InventoryTrans: "https://api.commerce7.com/v1/inventory-transaction",
+		Order:          "https://api.commerce7.com/v1/order",
+		Product:        "https://api.commerce7.com/v1/product",
 	}
-
-	return &endpoint
 }
