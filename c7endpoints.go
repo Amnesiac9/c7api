5d4e708a9d917724398506db7e103d668eb95b87
John <johnmoreau19@gmail.com>
2024-04-06 10:26:11 -0700

    Revert "Added const url endpoints"
    This reverts commit e3f034c3fe609a8b8941a222cc0df82ce3a1808e.

--- c7endpoints.go
+++ c7endpoints.go
@@ -1,25 +1,17 @@
 package c7api
 
-// var Endpoints struct {
-// 	Base         string // "https://api.commerce7.com/v1"
-// 	Auth         string // "https://api.commerce7.com/v1/account/user"
-// 	Cart         string // "https://api.commerce7.com/v1/cart"
-// 	Customer     string // "https://api.commerce7.com/v1/customer"
-// 	CustomerId   string // "https://api.commerce7.com/v1/customer/{:id}"
-// 	CustomerAddr string // "https://api.commerce7.com/v1/customer-address"
-// 	Order        string
-// } {}
+type Endpoint struct {
+	Auth     string
+	Customer string
+	Order    string
+}
 
-const (
-	Endpoint               string = "https://api.commerce7.com/v1"                             // https://developer.commerce7.com/docs/commerce7-apis
-	EndpointAuth           string = "https://api.commerce7.com/v1/account/user"                // https://developer.commerce7.com/docs/commerce7-apis
-	EndpointCart           string = "https://api.commerce7.com/v1/cart"                        // https://developer.commerce7.com/docs/orders
-	EndpointCustomer       string = "https://api.commerce7.com/v1/customer"                    // https://developer.commerce7.com/docs/customers
-	EndpointCustomerId     string = "https://api.commerce7.com/v1/customer/{:id}"              // https://developer.commerce7.com/docs/customers
-	EndpointFulfillmentAll string = "https://api.commerce7.com/v1/order/{:id}/fulfillment/all" // https://developer.commerce7.com/docs/fulfillment
-	EndpointFulfillment    string = "https://api.commerce7.com/v1/order/{:id}/fulfillment"     // https://developer.commerce7.com/docs/fulfillment
-	EndpointInventory      string = "https://api.commerce7.com/v1/inventory"                   // https://developer.commerce7.com/docs/inventory
-	EndpointInventoryTrans string = "https://api.commerce7.com/v1/inventory-transaction"       // https://developer.commerce7.com/docs/inventory
-	EndpointOrder          string = "https://api.commerce7.com/v1/order"                       // https://developer.commerce7.com/docs/orders
-	EndpointProduct        string = "https://api.commerce7.com/v1/product"                     // https://developer.commerce7.com/docs/products
-)
+func GetEndpointsV1() *Endpoint {
+	endpoint := Endpoint{
+		Auth:     "https://api.commerce7.com/v1/account/user",
+		Customer: "",
+		Order:    "",
+	}
+
+	return &endpoint
+}
