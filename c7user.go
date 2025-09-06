01ec1667612a548968d957bb4f97d276cbb32c77
John <johnmoreau19@gmail.com>
2025-09-05 23:02:41 -0700

    Fixed tenant id name and tags

--- c7user.go
+++ c7user.go
@@ -1,11 +1,11 @@
 package c7api
 
 type NewUser struct {
-	Tenant string `json:"tenantId"`
-	User   struct {
-		C7UserID  string `json:"id"`
-		FirstName string `json:"firstName" form:"firstName"`
-		LastName  string `json:"lastName" form:"lastName"`
-		Email     string `json:"email" form:"email"`
+	TenantID string `json:"tenantId"`
+	User     struct {
+		ID        string `json:"id"`
+		FirstName string `json:"firstName"`
+		LastName  string `json:"lastName"`
+		Email     string `json:"email"`
 	} `json:"user"`
 }
