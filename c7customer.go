eebf033fecd40bb3875e64039025f30a00ee24ed
John <johnmoreau19@gmail.com>
2025-09-12 15:45:33 -0700

    Add function to get C7 Customers in bulk

--- c7customer.go
+++ c7customer.go
@@ -63,3 +63,40 @@ func GetCustomerById[T HasEmails](customerId string, tenant string, c7AppAuthEnc
 
 	return &c7Customer, nil
 }
+
+func GetCustomersWithCursor[T HasEmails](tenant string, queries map[string]string, c7AppAuthEncoded string, retryCount int, rl genericRateLimiter) (*[]T, error) {
+
+	type CustomersCursor struct {
+		Customers []T
+		Cursor    string `json:"cursor"`
+	}
+
+	var c7Customers []T
+
+	cursor := "start"
+	reqUrl := Endpoints.Customer
+	if queries != nil {
+		queries["cursor"] = cursor
+	}
+
+	// Loop until cursor ends
+	for {
+		if cursor == "" {
+			break
+		}
+		resp, err := RequestWithRetryAndRead("GET", reqUrl, queries, nil, tenant, c7AppAuthEncoded, retryCount, rl)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get customer from tenant: %w", err)
+		}
+
+		var c7CustomerBatch CustomersCursor
+		if err := json.Unmarshal(*resp, &c7CustomerBatch); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal customer: %w", err)
+		}
+
+		c7Customers = append(c7Customers, c7CustomerBatch.Customers...)
+		cursor = c7CustomerBatch.Cursor
+	}
+
+	return &c7Customers, nil
+}
