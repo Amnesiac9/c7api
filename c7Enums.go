c0d609d5223580cf829fd2ffd2ddf81af937e768
John <johnmoreau19@gmail.com>
2025-11-07 21:35:00 -0800

    Added tax types, Web Statuse, Admin Status, and valid type check

--- c7Enums.go
+++ c7Enums.go
@@ -83,3 +83,69 @@ const (
 	TagXObjectTypeCustomer = "customer"
 	TagXObjectTypeOrder    = "order"
 )
+
+func IsValidTagXObjectType(objectType string) bool {
+	switch objectType {
+	case TagXObjectTypeCustomer,
+		TagXObjectTypeOrder:
+		return true
+	default:
+		return false
+	}
+}
+
+// Variant Tax Types
+const (
+	TaxTypeWine               = "Wine"
+	TaxTypeGeneralMerchandise = "General Merchandise"
+	TaxTypeFood               = "Food"
+	TaxTypePreparedFood       = "Prepared Food"
+	TaxTypeBooks              = "Books"
+	TaxTypeNotTaxable         = "Not Taxable"
+)
+
+func IsValidTaxType(taxType string) bool {
+	switch taxType {
+	case TaxTypeWine,
+		TaxTypeGeneralMerchandise,
+		TaxTypeFood,
+		TaxTypePreparedFood,
+		TaxTypeBooks,
+		TaxTypeNotTaxable:
+		return true
+	default:
+		return false
+	}
+}
+
+const (
+	AdminStatusAvailable    = "Available"
+	AdminStatusNotAvailable = "Not Available"
+)
+
+func IsValidAdminStatus(status string) bool {
+	switch status {
+	case AdminStatusAvailable,
+		AdminStatusNotAvailable:
+		return true
+	default:
+		return false
+	}
+}
+
+const (
+	WebStatusAvailable    = "Available"
+	WebStatusNotAvailable = "Not Available"
+	WebStatusRetired      = "Retired"
+)
+
+func IsValidWebStatus(status string) bool {
+	switch status {
+	case WebStatusAvailable,
+		WebStatusNotAvailable,
+		WebStatusRetired:
+		return true
+	default:
+		return false
+	}
+}
